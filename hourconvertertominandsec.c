b1f3826c88831b1c60ffef3287d0ddb9f10175cf
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-01-13 12:53:07 +0100

    Create hourconvertertominandsec.c

---
+++ hourconvertertominandsec.c
@@ -0,0 +1,21 @@
+/* Name: Nathália Pimentel de Assis
+  University: University of Brasilia
+  Bachelor student in eletronic engineering course
+  Date: June 18th 2019 */
+  
+  
+
+#include <stdio.h>
+
+  int main(){
+    long long int h,aux, min, seg;
+    scanf("%lld", &h);
+    min = h*60;
+    seg = min*60;
+
+    printf("%lld\n", min);
+    printf("%lld\n", seg);
+
+
+return 0;
+}
